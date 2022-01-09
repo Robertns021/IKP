7b1c1260edb60d584012aaa037074fbae62a87ef
Robert <molnarrobert99@yahoo.com>
2022-01-09 18:07:20 +0100

    Server settings

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -4,12 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Server
+namespace IKP
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /*while(true)
+            {
+                Neka logika da server uvek radi
+            }*/
+
+            Console.ReadKey();
         }
     }
 }
