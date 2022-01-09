daf60c72f4009651f7f4db6cdfa6a255c4005111
Robert <molnarrobert99@yahoo.com>
2022-01-09 18:12:36 +0100

    Klient
    Dodavanje klienta

---
+++ Server/Klient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome");
+
+            int x = -1;
+            while(x != 0)
+            {
+                Console.WriteLine();
+                /*
+                Opcije klijenta
+                */
+                x = int.Parse(Console.ReadLine());
+                /*
+                Logika klijenta primanje poruka citanje poruka                 
+                */
+            }
+            Console.WriteLine("Goodbye");
+            Console.ReadKey();
+        }
+    }
+}
